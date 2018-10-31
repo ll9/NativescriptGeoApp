60edc4c8d2fb3f409f93c5ab60a91a0911982c31
ll9 <llwierer@web.de>
2018-10-31 23:36:30 +0100

    loading openlayers html page from wwwroot

---
+++ app/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <Page class="page">
+        <ActionBar class="action-bar">
+            <Label class="action-bar-title" text="Home"></Label>
+        </ActionBar>
+        <WebView src="~/wwwroot/index.html"></WebView>
+    </Page>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {}
+        }
+    };
+</script>
+
+<style scoped lang="scss">
+    
+</style>
