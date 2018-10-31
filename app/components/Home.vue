ca4092687cae9a0498df34fd6ddc5f5bafb419d7
ll9 <llwierer@web.de>
2018-10-31 23:53:52 +0100

    positioned get location button

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -3,18 +3,43 @@
         <ActionBar class="action-bar">
             <Label class="action-bar-title" text="Home"></Label>
         </ActionBar>
-        <WebView src="~/wwwroot/index.html"></WebView>
+        <StackLayout>
+            <WebView src="~/wwwroot/index.html"></WebView>
+            <Button text="Get location" @tap="buttonGetLocationTap"></Button>
+        </StackLayout>
     </Page>
 </template>
 
 <script>
-    export default {
-        data: function() {
-            return {}
-        }
-    };
+var geolocation = require("nativescript-geolocation");
+
+export default {
+  data: function() {
+    return {};
+  },
+  methods: {
+    buttonGetLocationTap(args) {
+      var location = geolocation
+        .getCurrentLocation({
+          desiredAccuracy: 3,
+          updateDistance: 10,
+          maximumAge: 20000,
+          timeout: 20000
+        })
+        .then(
+          function(loc) {
+            if (loc) {
+              console.log("Current location is: " + JSON.stringify(loc));
+            }
+          },
+          function(e) {
+            console.log("Error: " + e.message);
+          }
+        );
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
-    
 </style>
