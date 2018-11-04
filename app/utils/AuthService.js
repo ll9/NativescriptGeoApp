132ae68035460fa194ca165c0c11db02e1e9c15d
ll9 <llwierer@web.de>
2018-11-04 22:03:18 +0100

    getting token on register

--- app/utils/AuthService.js
+++ app/utils/AuthService.js
@@ -1,6 +1,8 @@
-const BASE_URL = '';
-const LOGIN_URL = '';
-const REGISTER_URL = '';
+const BASE_URL = 'http://192.168.178.42:3000';
+const LOGIN_URL = BASE_URL + '/account/login';
+const REGISTER_URL = BASE_URL + '/account/register';
+
+import * as http from 'http';
 
 export default class AuthService {
     login(email, password) {
@@ -8,6 +10,25 @@ export default class AuthService {
     }
 
     register(email, password) {
-        alert('register');
+        console.log(REGISTER_URL);
+        alert(REGISTER_URL);
+        http.request({
+            url: REGISTER_URL,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            content: JSON.stringify({
+                email,
+                password
+            })
+        }).then((response) => {
+            let token = response.content.toString();
+            console.log(token);
+            alert(token)
+        }, (e) => {
+            alert(JSON.stringify(e))
+            console.log(e)
+        });
     }
 }
